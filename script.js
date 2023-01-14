07497694091eb8077d9369d838590e6a0354519b
Jsheen98 <jsheendev@gmail.com>
2023-01-14 11:18:02 -0700

    Completed JS file, README, HTML/CSS

--- script.js
+++ script.js
@@ -1,69 +1,66 @@
-var dateDisplayEl = $('#currentDay')
-var timeDisplayEl = $('#currentTime')
-var rightNow = dayjs().format('H')
+var rootEl = $("#root");
+var dateDisplayEl = $("#currentDay");
+var timeDisplayEl = $("#currentTime");
+var rightNow = dayjs().format("H");
+var userInput = $(".description");
 
+// Array for work hours (these are my normal work hours 8-4)
 var workHours = [
-  hour8El = $('#hour-8'),
-  hour9El = $('#hour-9'),
-  hour10El = $('#hour-10'),
-  hour11El = $('#hour-11'),
-  hour12El = $('#hour-12'),
-  hour13El = $('#hour-13'),
-  hour14El = $('#hour-14'),
-  hour15El = $('#hour-15'),
-  hour16El = $('#hour-16')
-]
+  (hour8El = $("#hour-8")),
+  (hour9El = $("#hour-9")),
+  (hour10El = $("#hour-10")),
+  (hour11El = $("#hour-11")),
+  (hour12El = $("#hour-12")),
+  (hour13El = $("#hour-13")),
+  (hour14El = $("#hour-14")),
+  (hour15El = $("#hour-15")),
+  (hour16El = $("#hour-16")),
+];
 
 // (document).ready() function to ensure the page loads first before any jQuery starts functioning
 $(function () {
 
-  // Function that displays the date and time at the top of the page
-  function displayDateAndTime() {
-    var todaysDate = dayjs().format('📅 dddd, MMMM D')
-    var currentTimeDisplay = dayjs().format('🕗 h:m a')
-    timeDisplayEl.text(currentTimeDisplay)
-    dateDisplayEl.text(todaysDate)
+  // Function that displays the date at the top of the page
+  function displayDate() {
+    var todaysDate = dayjs().format("📅 dddd, MMMM D");
+    dateDisplayEl.text(todaysDate);
   }
 
   // Function that loops through the workHours array, and sets the color theme for the hour block depending on the added class (past, present, or future)
   function colorRows() {
-
     for (var i = 0; i < workHours.length; i++) {
-      var blockHour = parseInt(
-        $(workHours[i]).attr('id').split('-')[1]
-      )
+      var blockHour = parseInt($(workHours[i]).attr("id").split("-")[1]);
       if (blockHour < rightNow) {
-        $(workHours[i]).addClass('past')
+        $(workHours[i]).addClass("past");
       } else if (blockHour == rightNow) {
-        $(workHours[i]).removeClass('past')
-        $(workHours[i]).addClass('present')
+        $(workHours[i]).removeClass("past");
+        $(workHours[i]).addClass("present");
       } else {
-        $(workHours[i]).removeClass('past')
-        $(workHours[i]).removeClass('present')
-        $(workHours[i]).addClass('future')
+        $(workHours[i]).removeClass("past");
+        $(workHours[i]).removeClass("present");
+        $(workHours[i]).addClass("future");
       }
     }
   }
 
   // Function to save input text to local storage
-  function saveText() {
-    
-    
+  $(".saveBtn").on ("click", function (event) {
+    event.preventDefault()
+    localStorage.setItem(
+      this.parentElement.id,
+      this.previousElementSibling.value
+    );
+  });
+
+  // Function that loads in saved local storage items
+  function textAreaFill() {
+    for (var i = 0; i < userInput.length; i++) {
+      userInput[i].value = (localStorage.getItem(userInput[i].parentElement.id))
+    }
+     
   }
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  colorRows()
-  displayDateAndTime();
-
+  textAreaFill();
+  colorRows();
+  displayDate();
 });
-
